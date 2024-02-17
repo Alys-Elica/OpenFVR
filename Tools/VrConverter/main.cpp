d3917f9c7f49f667b0d029738e78f395237e2c5a
Alys Elica <aelica@nephria.fr>
2024-02-17 08:44:20 +0100

    [tools] Re-enabled FnxVr viewer

--- Tools/VrConverter/main.cpp
+++ Tools/VrConverter/main.cpp
@@ -6,8 +6,6 @@
 
 #include "fvr_files/fvr_vr.h"
 
-#if defined(HAS_FNXVR)
-
 #include "fnxvr.h"
 
 int vrViewer(const std::string &vrFileName)
@@ -48,15 +46,6 @@ int vrViewer(const std::string &vrFileName)
 
     return 0;
 }
-#else
-
-int vrViewer(const std::string &vrFileName)
-{
-    std::cerr << "This tool was compiled without VR viewer support" << std::endl;
-    return 1;
-}
-
-#endif
 
 int vrConvert(const std::string &vrFileName, const bool toCubemap)
 {
@@ -120,7 +109,7 @@ int main(int argc, char *argv[])
     optGeneric.add_options()("vr-animation", boost::program_options::value<std::string>(), "converts the given VR file animations to a WebP animated image");
     optGeneric.add_options()("vr-animation-cubemap", boost::program_options::value<std::string>(), "converts the given VR file to a cubemap WebP animated image");
     optGeneric.add_options()("vr-viewer", boost::program_options::value<std::string>(), "view the given VR file");
-    
+
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(optGeneric).run(), vm);
     boost::program_options::notify(vm);
